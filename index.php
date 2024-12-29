f5696a4432f6ce9dd3416c19033394be23ec8b3e
Gama Anggadipa Pratama <gamaanggadipapratama@gmail.com>
2024-12-29 16:15:40 +0700

    My first PHP tags

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Demo</title>
+  </head>
+  <body>
+    <h1>
+      <?php echo "Hello, World"; ?>
+    </h1>
+    <p><?php echo "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam, accusantium." ?></p>
+  </body>
+</html>
