0372a2b79f5ad3cc16ba0f2b527b6611bbd7e564
Gama Anggadipa Pratama <gamaanggadipapratama@gmail.com>
2024-12-29 17:24:32 +0700

    Learn Conditionals and Booleans

--- index.php
+++ index.php
@@ -2,13 +2,29 @@
 <html lang="en">
   <head>
     <title>Demo</title>
+    <style>
+      body {
+        display: grid;
+        place-items: center;
+        height: 100vh;
+        margin: 0;
+        font-family: sans-serif;
+      }
+    </style>
   </head>
   <body>
+    <?php 
+      $name = "Dark Matter";
+      $read = false;
+
+      if($read) {
+        $message = "You have read \"$name\".";
+      } else {
+        $message = "You have NOT read \"$name\".";
+      }
+    ?>
     <h1>
-      <?php
-        $greeting = "Hello";
-        echo "$greeting World"; 
-      ?>
+      <?= $message ?>
     </h1>
   </body>
 </html>
