e46c0a46440abce3c48b23199dd38fc688debb2a
Gama Anggadipa Pratama <gamaanggadipapratama@gmail.com>
2024-12-30 12:52:04 +0700

    Learn Anonymous Function

--- index.php
+++ index.php
@@ -10,22 +10,51 @@
         [
           "name" => "Do Androids Dream of Electric Sheep",
           "author" => "Philip K. Dick",
+          "releaseYear" => 1968,
           "purchaseUrl" => "http://example.com"
         ],
         [
           "name" => "Project Hail Mary",
           "author" => "Andy Weir",
+          "releaseYear" => 2021,
+          "purchaseUrl" => "http://example.com"
+        ],
+        [
+          "name" => "The Martian",
+          "author" => "Andy Weir",
+          "releaseYear" => 2011,
           "purchaseUrl" => "http://example.com"
         ]
       ];
+
+      function filter($items, $fn) {
+        $filteredItems = [];
+
+        foreach($items as $item) {
+          if($fn($item)) {
+            $filteredItems[] = $item;
+          }
+        }
+        
+        return $filteredItems;
+      }
+
+      $filteredBooks = filter($books, function($book) {
+        return $book['releaseYear'] < 2000;
+      });
+
+      // Using PHP built-in function
+      $filteredBooks = array_filter($books, function($book) {
+        return $book['releaseYear'] < 2000;
+      });
     ?>
     <ul>
-      <?php foreach($books as $book) : ?>
-          <li>
-            <a href="<?= $book['purchaseUrl'] ?>">
-              <?= $book['name'] ?>
-            </a>
-          </li>
+      <?php foreach($filteredBooks as $book) : ?>
+        <li>
+          <a href="<?= $book['purchaseUrl'] ?>">
+            <?= $book['name'] ?> (<?= $book['releaseYear'] ?>)
+          </a>
+        </li>
       <?php endforeach; ?>
     </ul>
   </body>
