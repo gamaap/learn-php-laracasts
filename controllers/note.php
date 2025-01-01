e45c1961cd81611495c5e3e71372c8c01d5c3f47
Gama Anggadipa Pratama <gamaanggadipapratama@gmail.com>
2025-01-01 15:04:46 +0700

    Learn about Authorization

--- controllers/note.php
+++ controllers/note.php
@@ -4,7 +4,18 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = 'My Notes';
+$currentUserId = 4;
 
-$note = $db->query("SELECT * FROM notes WHERE id = :id", [':id' => $_GET['id']])->fetch();
+$note = $db->query("SELECT * FROM notes WHERE id = :id", [
+  ':id' => $_GET['id']
+])->fetch();
+
+if (!$note) {
+  abort();
+}
+
+if ($note['user_id'] !== $currentUserId) {
+  abort(Response::FORBIDDEN);
+}
 
 require 'views/note.view.php';
