8f793dbbad7334e7da0324f0100238b5b2f53dac
Gama Anggadipa Pratama <gamaanggadipapratama@gmail.com>
2025-01-01 17:00:32 +0700

    learn how to always escape untrusted input

--- views/note.view.php
+++ views/note.view.php
@@ -7,7 +7,7 @@
     <p class="mb-5">
       <a href="/notes" class="text-blue-500 underline">Go Back...</a>
     </p>
-    <p><?= $note['body'] ?></p>
+    <p><?= htmlspecialchars($note['body']) ?></p>
   </div>
 </main>
 
