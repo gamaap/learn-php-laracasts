c679cf0ee04ba08076433548c7b1947702ee86b3
Gama Anggadipa Pratama <gamaanggadipapratama@gmail.com>
2025-01-01 16:50:32 +0700

    Adding forms and learn request method

--- router.php
+++ router.php
@@ -1,14 +1,6 @@
-<?php 
+<?php
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-$routes = [
-  '/' => 'controllers/index.php',
-  '/about' => 'controllers/about.php',
-  '/notes' => 'controllers/notes.php',
-  '/note' => 'controllers/note.php',
-  '/contact' => 'controllers/contact.php'
-];
+$routes = require("routes.php");
 
 function routeToController($uri, $routes) {
   if(array_key_exists($uri, $routes)) {
@@ -24,4 +16,5 @@ function abort($code = 404) {
   die();
 }
 
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 routeToController($uri, $routes);
