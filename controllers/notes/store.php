7b8f0fe02f5c89ccd4f0d9aed31500139631cb0f
Gama Anggadipa Pratama <gamaanggadipapratama@gmail.com>
2025-01-05 19:59:56 +0700

    Make my first service container

--- controllers/notes/store.php
+++ controllers/notes/store.php
@@ -1,12 +1,12 @@
 <?php
 
-use Core\Database;
+use Core\App;
 use Core\Validator;
+use Core\Database;
+
+$db = App::resolve(Database::class);
 
 $errors = [];
-
-$config = require base_path('config.php');
-$db = new Database($config['database']);
 
 if (!Validator::string($_POST['body'], 1, 1000)) {
   $errors['body'] = 'A body of no more than 1,000 characters is required.';
