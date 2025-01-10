394fb1f709bf244a5bad07c43cb273344e732474
Gama Anggadipa Pratama <gamaanggadipapratama@gmail.com>
2025-01-10 11:09:36 +0700

    Extract an Authentication class

--- Http/controllers/sessions/store.php
+++ Http/controllers/sessions/store.php
@@ -1,34 +1,23 @@
 <?php
 
-use Core\App;
-use Core\Database;
-use Core\Validator;
+use Core\Authenticator;
 use Http\Forms\LoginForm;
-
-$db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 $form = new LoginForm();
-if(!$form->validate($email, $password)) {
-  return view('sessions/create.view.php', [
-    'errors' => $form->errors()
-  ]);
-}
 
-// match the credential
-$user = $db->query("SELECT email, password FROM users WHERE email = :email", [
-  ':email' => $email
-])->find();
-
-if ($user) {
-  if (password_verify($password, $user['password'])) {
-    login([
-      'email' => $email
-    ]);
+if($form->validate($email, $password)) {
+  $auth = new Authenticator;
   
-    header('location: /');
-    exit();
+  if($auth->attempt($email, $password)) {
+    redirect("/");
   }
+    
+  $form->error('email', 'No matching account found for that email address and password.');
 }
+
+return view('sessions/create.view.php', [
+  'errors' => $form->errors()
+]);
