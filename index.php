ccbe004d4ada7dbc1367de131623bb74a09a14c3
Gama Anggadipa Pratama <gamaanggadipapratama@gmail.com>
2025-06-01 22:33:03 +0700

    conditionals and booleans

--- index.php
+++ index.php
@@ -4,13 +4,29 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Demo</title>
+    <style>
+      body {
+        display: grid;
+        place-items: center;
+        height: 100vh;
+        margin: 0;
+        font-family: sans-serif;
+      }
+    </style>
   </head>
   <body>
+    <?php 
+      $title = "Dark Matter";
+      $read = false;
+
+      if ($read) {
+        $message = "You have read \"$title\".";
+      } else {
+        $message = "You have NOT read \"$title\".";
+      }
+    ?>
     <h1>
-      <?php
-        $greeting = "Hello";
-        echo "$greeting Everybody!";
-      ?>
+      <?= $message ?>
     </h1>
   </body>
 </html>
