b9cbb34e17879e46d02395ac638426200eb3996f
Gama Anggadipa Pratama <gamaanggadipapratama@gmail.com>
2025-06-01 22:42:06 +0700

    My First Arrays

--- index.php
+++ index.php
@@ -4,29 +4,22 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Demo</title>
-    <style>
-      body {
-        display: grid;
-        place-items: center;
-        height: 100vh;
-        margin: 0;
-        font-family: sans-serif;
-      }
-    </style>
   </head>
   <body>
-    <?php 
-      $title = "Dark Matter";
-      $read = false;
+    <h1>Recommended Books</h1>
 
-      if ($read) {
-        $message = "You have read \"$title\".";
-      } else {
-        $message = "You have NOT read \"$title\".";
-      }
+    <?php 
+      $books = [
+        'Do Androids Dream of Electric Sheep',
+        'The Langoliers',
+        'Hail Mary'
+      ];
     ?>
-    <h1>
-      <?= $message ?>
-    </h1>
+
+    <ul>
+      <?php foreach ($books as $book) : ?>
+        <li><?= $book ?></li>
+      <?php endforeach ?>
+    </ul>
   </body>
 </html>
