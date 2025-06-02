3dc215ff97d9d465dbb92a6e26f545140cc8671a
Gama Anggadipa Pratama <gamaanggadipapratama@gmail.com>
2025-06-02 22:03:31 +0700

    Make a PHP Router

---
+++ views/404.php
@@ -0,0 +1,13 @@
+<?php require 'partials/head.php' ?>
+<?php require 'partials/nav.php' ?>
+      
+<main>
+  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <p class="mb-5">
+      <a href="/" class="text-blue-500 hover:underline">Go Back</a>
+    </p>
+    <h1 class="text-2xl font-bold">Sorry. Page Not Found.</h1>
+  </div>
+</main>
+
+<?php require 'partials/footer.php' ?>
