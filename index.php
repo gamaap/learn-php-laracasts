1a758ed8ac9e71a5717666ffac068175c1fd8d7b
Gama Anggadipa Pratama <gamaanggadipapratama@gmail.com>
2025-06-02 21:08:09 +0700

    Create Page Links

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
 <?php 
 
-$books = [
-  [
-    'title' => 'Do Androids Dream of Electric Sheep',
-    'author' => 'Philip K. Dick',
-    'releaseYear' => 1968,
-    'purchaseUrl' => 'http://example.com'
-  ],
-  [
-    'title' => 'Project Hail Mary',
-    'author' => 'Andy Weir',
-    'releaseYear' => 2021,
-    'purchaseUrl' => 'http://example.com'
-  ],
-  [
-    'title' => 'The Martian',
-    'author' => 'Andy Weir',
-    'releaseYear' => 2011,
-    'purchaseUrl' => 'http://example.com'
-  ],
-];
-
-$filteredBooks = array_filter($books, function($book) {
-  return $book['author'] === 'Andy Weir';
-});
-
 require 'index.view.php';
