68f0037e11801a47d43cdbec3c3d426d9b4196d1
Gama Anggadipa Pratama <gamaanggadipapratama@gmail.com>
2025-06-03 21:38:48 +0700

    Render the notes and note page

---
+++ controllers/notes.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = require 'config.php';
+$db = new Database($config['database']);
+
+$heading = "My Notes";
+
+$currentUserId = $db->query("SELECT id FROM users WHERE name = 'John'")->fetch();
+
+$notes = $db->query("SELECT * FROM notes WHERE user_id = " . $currentUserId['id'])->fetchAll();
+
+require 'views/notes.view.php';
--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php 
 
 require 'functions.php';
-// require 'router.php';
 require 'Database.php';
+require 'router.php';
 
 $config = require 'config.php';
-
-$id = $_GET['id'];
-
 $db = new Database($config['database']);
-$post = $db->query("SELECT * FROM posts WHERE id = :id", ['id' => $id])->fetch();
-
-dd($post);
